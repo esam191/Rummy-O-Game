a0893329ab429347a9ee06143cff71b022741e62
Esam Uddin <60678042+esam191@users.noreply.github.com>
2020-04-03 10:03:06 -0400

    updates unit tests

--- test cases/unit_testing/__tests__/filterByTerm.spec.js
+++ test cases/unit_testing/__tests__/filterByTerm.spec.js
@@ -1,36 +1,36 @@
 describe("Filter function", () => {
-  test("it should filter by a search term (link)", () => {
+  test("Check all independent paths through the control structure to ensure that all statements in the Room module have been executed at least once (search term: data)", () => {
     const input = [
-      { id: 1, url: "https://www.url1.dev" },
-      { id: 2, url: "https://www.url2.dev" },
-      { id: 3, url: "link between data : transferable" }
+     { id: 1, game: "module operates correctly" },
+      { id: 2, game: "path executes with no error" },
+      { id: 3, game: "data transfered" }
     ];
 
-    const output = [{ id: 3, url: "link between data : transferable" }];
+    const output = [{ id: 3, game: "data transfered" }];
 
-    expect(filterByTerm(input, "link")).toEqual(output);
+    expect(filterByTerm(input, "data")).toEqual(output);
 
-    expect(filterByTerm(input, "LINK")).toEqual(output);
+    expect(filterByTerm(input, "DATA")).toEqual(output);
   });
 
-  test("it should filter by a search term (uRl)", () => {
+  test("Test boundary conditions to ensure that the interface and room module operates properly at boundaries established to restrict processing. (search term : data)", () => {
     const input = [
-      { id: 1, url: "deployment url functions" },
-      { id: 2, url: "url running" },
-      { id: 3, url: "https://www.link3.dev" }
+      { id: 1, game: "data error description is intelligible" },
+      { id: 2, game: "data enters and exits properly" },
+      { id: 3, game: "all statements are executed at least once " }
     ];
 
-    const output = [{ id: 1, url: "deployment url functions" },
-            { id: 2, url: "url running" }];
+    const output = [{ id: 1, game: "data error description is intelligible" },
+            { id: 2, game: "data enters and exits properly" }];
 
-    expect(filterByTerm(input,"uRl")).toEqual(output);
+    expect(filterByTerm(input,"data")).toEqual(output);
   });
 
   test("it should throw error if there is an empty search term", () => {
     const input = [];
 
-    const output = [{ id: 1, url: "https://www.url1.dev" },
-            { id: 2, url: "https://www.url2.dev" }];
+    const output = [{ id: 1, game: "https://www.url1.dev" },
+            { id: 2, game: "https://www.url2.dev" }];
 
     expect(filterByTerm(input,""));
   });
@@ -40,6 +40,6 @@ describe("Filter function", () => {
 function filterByTerm(inputArr, searchTerm) {
   const regex = new RegExp(searchTerm, "i");
   return inputArr.filter(function(arrayElement) {
-    return arrayElement.url.match(regex);
+    return arrayElement.game.match(regex);
   });
 }
